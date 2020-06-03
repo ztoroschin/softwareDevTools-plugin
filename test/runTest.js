33e6a7795af023a901c1ea23d248a619541ca8c2
Zahar Toroschin <z.toroschin@gmail.com>
2020-06-03 18:30:39 +0300

    Fix github actions configuration

--- test/runTest.js
+++ test/runTest.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const { runTests } = require('vscode-test');
 
-async function main() {
+function main() {
 	try {
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
@@ -13,7 +13,7 @@ async function main() {
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		runTests({ extensionDevelopmentPath, extensionTestsPath });
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
